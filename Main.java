9ddf47907e3ff094c7c4aa16afc046e37e990868
NutchapolSal <Nutchapol.sal@gmail.com>
2023-12-28 17:08:58 +0700

    added image type & editor

--- Main.java
+++ Main.java
@@ -1,19 +1,23 @@
+import java.awt.AlphaComposite;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.Polygon;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
 
+import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.GroupLayout;
 import javax.swing.JColorChooser;
@@ -22,7 +26,9 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JSlider;
 import javax.swing.JSpinner;
+import javax.swing.JTextField;
 import javax.swing.SpinnerNumberModel;
 import javax.swing.Timer;
 import javax.swing.GroupLayout.Alignment;
@@ -32,6 +38,8 @@ public class Main {
     public static void main(String[] args) {
 
         List<GraphicLayer> instructions = new ArrayList<>();
+        instructions.add(new GraphicLayer("base sketch", new ArrayList<>())
+                .add(new GraphicImage("baseSketch.jpg", new Point(0, 0), new Dimension(600, 600), 1.0)));
         instructions.add(new GraphicLayer("cross", new ArrayList<>())
                 .add(new GraphicLine("#777700", new Point(50, 50), new Point(550, 550)))
                 .add(new GraphicLine("#777700", new Point(50, 550), new Point(550, 50))));
@@ -58,7 +66,7 @@ public class Main {
 
         JFrame frame2 = new JFrame();
         JScrollPane scrollPane = new JScrollPane();
-        scrollPane.setViewportView(EditingPanelFactory.create(instructions.get(1)));
+        scrollPane.setViewportView(EditingPanelFactory.create(instructions.get(0)));
         frame2.add(scrollPane);
         frame2.setLocation(frame.getLocation().x + frame.getWidth(), frame.getLocation().y);
         // EditingPanelFactory.recurseAddBorders(frame2.getContentPane());
@@ -69,6 +77,28 @@ public class Main {
         new Timer(1000 / 60, e -> {
             panel.repaint();
         }).start();
+
+    }
+}
+
+class MutableDouble {
+    public double value;
+
+    MutableDouble(double value) {
+        this.value = value;
+    }
+}
+
+class MutableString {
+    public String value;
+
+    MutableString(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
 
     }
 }
@@ -99,16 +129,19 @@ class GraphicLayer {
 }
 
 abstract class GraphicObject {
+    abstract public void draw(BufferedImage buffer);
+}
+
+abstract class GraphicPlotter extends GraphicObject {
     public Color color = Color.black;
 
     protected void plot(Graphics g, int x, int y) {
         g.fillRect(x, y, 1, 1);
     }
 
-    abstract public void draw(BufferedImage buffer);
 }
 
-class GraphicLine extends GraphicObject {
+class GraphicLine extends GraphicPlotter {
     public Point p1, p2;
 
     GraphicLine(String hexColor, Point p1, Point p2) {
@@ -160,7 +193,7 @@ class GraphicLine extends GraphicObject {
     }
 }
 
-class GraphicPolygon extends GraphicObject {
+class GraphicPolygon extends GraphicPlotter {
     public List<Point> points;
 
     GraphicPolygon(String hexColor, List<Point> points) {
@@ -181,7 +214,7 @@ class GraphicPolygon extends GraphicObject {
     }
 }
 
-class GraphicBezierCurve extends GraphicObject {
+class GraphicBezierCurve extends GraphicPlotter {
     private static final int BEZIER_ITERATIONS = 2000;
     public Point p1, p2, p3, p4;
 
@@ -216,7 +249,7 @@ class GraphicBezierCurve extends GraphicObject {
     }
 }
 
-class GraphicFloodFill extends GraphicObject {
+class GraphicFloodFill extends GraphicPlotter {
     public Point point;
 
     GraphicFloodFill(String hexColor, Point point) {
@@ -249,23 +282,70 @@ class GraphicFloodFill extends GraphicObject {
     }
 }
 
+class GraphicImage extends GraphicObject {
+    public MutableString filePath;
+    private String lastFilePath = "";
+    public BufferedImage image;
+    public Point origin;
+    public Dimension size;
+    public MutableDouble opacity;
+
+    GraphicImage(String filePath, Point origin, Dimension size, double opacity) {
+        this.filePath = new MutableString(filePath);
+        this.origin = origin;
+        this.size = size;
+        this.opacity = new MutableDouble(opacity);
+        updateImage();
+    }
+
+    private void updateImage() {
+        if (lastFilePath.equals(filePath.value)) {
+            return;
+        }
+        try {
+            this.image = ImageIO.read(new File(this.filePath.value));
+        } catch (Exception e) {
+            this.image = new BufferedImage(50, 50, BufferedImage.TYPE_INT_ARGB);
+            Graphics g = image.createGraphics();
+            g.setColor(Color.red);
+            g.fillRect(0, 0, 50, 50);
+            g.setColor(Color.white);
+            g.drawString("ERROR", 0, 0);
+        }
+        lastFilePath = filePath.value;
+    }
+
+    @Override
+    public void draw(BufferedImage buffer) {
+        Graphics2D g = (Graphics2D) buffer.createGraphics();
+
+        updateImage();
+
+        AlphaComposite alphaComposite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, (float) opacity.value);
+        g.setComposite(alphaComposite);
+        g.drawImage(image, origin.x, origin.y, size.width, size.height, null);
+    }
+}
+
 class EditingPanelFactory {
     private EditingPanelFactory() {
     }
 
     public static JPanel create(String labelText, Point point) {
-
         JPanel panel = new JPanel();
         JLabel label = new JLabel(labelText);
         GroupLayout layout = new GroupLayout(panel);
         panel.setLayout(layout);
 
-        JSpinner xSpinner = new JSpinner(new SpinnerNumberModel(point.x, 0, 599, 1));
+        SpinnerNumberModel xModel = new SpinnerNumberModel();
+        xModel.setValue(point.x);
+        SpinnerNumberModel yModel = new SpinnerNumberModel();
+        yModel.setValue(point.y);
+        JSpinner xSpinner = new JSpinner(xModel);
         xSpinner.addChangeListener(e -> {
             point.x = (int) xSpinner.getValue();
         });
-
-        JSpinner ySpinner = new JSpinner(new SpinnerNumberModel(point.y, 0, 599, 1));
+        JSpinner ySpinner = new JSpinner(yModel);
         ySpinner.addChangeListener(e -> {
             point.y = (int) ySpinner.getValue();
         });
@@ -301,8 +381,7 @@ class EditingPanelFactory {
 
             @Override
             public void mouseDragged(java.awt.event.MouseEvent e) {
-                point.x = originX + (e.getX() - startX);
-                xSpinner.setValue(point.x);
+                xSpinner.setValue(originX + (e.getX() - startX));
             }
 
             @Override
@@ -338,8 +417,7 @@ class EditingPanelFactory {
 
             @Override
             public void mouseDragged(java.awt.event.MouseEvent e) {
-                point.y = originY + (e.getY() - startY);
-                ySpinner.setValue(point.y);
+                ySpinner.setValue(originY + (e.getY() - startY));
             }
 
             @Override
@@ -400,6 +478,209 @@ class EditingPanelFactory {
         return panel;
     }
 
+    public static JPanel create(String labelText, Dimension dim) {
+        JPanel panel = new JPanel();
+        JLabel label = new JLabel(labelText);
+        GroupLayout layout = new GroupLayout(panel);
+        panel.setLayout(layout);
+
+        SpinnerNumberModel wModel = new SpinnerNumberModel();
+        wModel.setValue(dim.width);
+        SpinnerNumberModel hModel = new SpinnerNumberModel();
+        hModel.setValue(dim.height);
+        JSpinner wSpinner = new JSpinner(wModel);
+
+        wSpinner.addChangeListener(e -> {
+            dim.width = (int) wSpinner.getValue();
+        });
+
+        JSpinner hSpinner = new JSpinner(hModel);
+        hSpinner.addChangeListener(e -> {
+            dim.height = (int) hSpinner.getValue();
+        });
+
+        JPanel pannerPanel = new JPanel();
+        pannerPanel.setPreferredSize(new Dimension(20, 20));
+        pannerPanel.setMaximumSize(new Dimension(20, 20));
+        pannerPanel.setMinimumSize(new Dimension(20, 20));
+        pannerPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        pannerPanel.setCursor(Cursor.getPredefinedCursor(Cursor.SE_RESIZE_CURSOR));
+
+        JPanel pannerXPanel = new JPanel();
+        pannerXPanel.setPreferredSize(new Dimension(20, 8));
+        pannerXPanel.setMaximumSize(new Dimension(20, 8));
+        pannerXPanel.setMinimumSize(new Dimension(20, 8));
+        pannerXPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        pannerXPanel.setCursor(Cursor.getPredefinedCursor(Cursor.E_RESIZE_CURSOR));
+
+        JPanel pannerYPanel = new JPanel();
+        pannerYPanel.setPreferredSize(new Dimension(8, 20));
+        pannerYPanel.setMaximumSize(new Dimension(8, 20));
+        pannerYPanel.setMinimumSize(new Dimension(8, 20));
+        pannerYPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        pannerYPanel.setCursor(Cursor.getPredefinedCursor(Cursor.S_RESIZE_CURSOR));
+
+        class PannerPanelXListener implements MouseMotionListener, MouseListener {
+            private int startX = 0;
+            private int originX = 0;
+
+            @Override
+            public void mouseMoved(java.awt.event.MouseEvent e) {
+            }
+
+            @Override
+            public void mouseDragged(java.awt.event.MouseEvent e) {
+                wSpinner.setValue(originX + (e.getX() - startX));
+            }
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                startX = e.getX();
+                originX = dim.width;
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+            }
+
+        }
+        class PannerPanelYListener implements MouseMotionListener, MouseListener {
+            private int startY = 0;
+            private int originY = 0;
+
+            @Override
+            public void mouseMoved(java.awt.event.MouseEvent e) {
+            }
+
+            @Override
+            public void mouseDragged(java.awt.event.MouseEvent e) {
+                hSpinner.setValue(originY + (e.getY() - startY));
+            }
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                startY = e.getY();
+                originY = dim.height;
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+            }
+
+        }
+
+        var xListener = new PannerPanelXListener();
+        var yListener = new PannerPanelYListener();
+        pannerPanel.addMouseListener(xListener);
+        pannerPanel.addMouseListener(yListener);
+        pannerPanel.addMouseMotionListener(xListener);
+        pannerPanel.addMouseMotionListener(yListener);
+        pannerXPanel.addMouseListener(xListener);
+        pannerXPanel.addMouseMotionListener(xListener);
+        pannerYPanel.addMouseListener(yListener);
+        pannerYPanel.addMouseMotionListener(yListener);
+
+        layout.setHorizontalGroup(layout.createSequentialGroup()
+                .addComponent(label)
+                .addPreferredGap(ComponentPlacement.RELATED)
+                .addComponent(wSpinner)
+                .addComponent(hSpinner)
+                .addPreferredGap(ComponentPlacement.RELATED)
+                .addComponent(pannerYPanel)
+                .addGroup(layout.createParallelGroup(Alignment.CENTER)
+                        .addComponent(pannerPanel)
+                        .addComponent(pannerXPanel)));
+        layout.setVerticalGroup(
+                layout.createSequentialGroup()
+                        .addComponent(pannerXPanel)
+                        .addGroup(layout.createParallelGroup(Alignment.BASELINE)
+                                .addComponent(label)
+                                .addComponent(wSpinner)
+                                .addComponent(hSpinner)
+                                .addComponent(pannerYPanel)
+                                .addComponent(pannerPanel)));
+
+        return panel;
+    }
+
+    public static JPanel create(String labelText, MutableString str) {
+        JPanel panel = new JPanel();
+        JLabel label = new JLabel(labelText);
+        GroupLayout layout = new GroupLayout(panel);
+        panel.setLayout(layout);
+
+        JTextField textField = new JTextField(str.value);
+        textField.addActionListener(e -> {
+            str.value = textField.getText();
+        });
+
+        layout.setHorizontalGroup(layout.createSequentialGroup()
+                .addComponent(label)
+                .addPreferredGap(ComponentPlacement.RELATED)
+                .addComponent(textField));
+        layout.setVerticalGroup(
+                layout.createParallelGroup(Alignment.BASELINE)
+                        .addComponent(label)
+                        .addComponent(textField));
+
+        return panel;
+    }
+
+    public static JPanel create(String labelText, MutableDouble doub, double min, double max, double stepSize) {
+        JPanel panel = new JPanel();
+        JLabel label = new JLabel(labelText);
+        GroupLayout layout = new GroupLayout(panel);
+        panel.setLayout(layout);
+
+        int sliderSteps = (int) ((max - min) / stepSize);
+        JSlider slider = new JSlider(0, sliderSteps, (int) ((doub.value - min) / stepSize));
+        JSpinner spinner = new JSpinner(new SpinnerNumberModel(doub.value, min, max, stepSize));
+        spinner.addChangeListener(e -> {
+            doub.value = (double) spinner.getValue();
+            slider.setValue((int) ((doub.value - min) / stepSize));
+        });
+        slider.addChangeListener(e -> {
+            spinner.setValue(slider.getValue() * stepSize + min);
+        });
+
+        layout.setHorizontalGroup(layout.createSequentialGroup()
+                .addComponent(label)
+                .addPreferredGap(ComponentPlacement.RELATED)
+                .addComponent(slider)
+                .addComponent(spinner));
+        layout.setVerticalGroup(
+                layout.createParallelGroup(Alignment.BASELINE)
+                        .addComponent(label)
+                        .addComponent(slider)
+                        .addComponent(spinner));
+
+        return panel;
+    }
+
     public static JPanel create(GraphicLayer layer) {
         JPanel panel = new JPanel();
         GroupLayout layout = new GroupLayout(panel);
@@ -431,6 +712,8 @@ class EditingPanelFactory {
             return create((GraphicBezierCurve) object);
         } else if (object instanceof GraphicFloodFill) {
             return create((GraphicFloodFill) object);
+        } else if (object instanceof GraphicImage) {
+            return create((GraphicImage) object);
         } else {
             return new JPanel();
         }
@@ -452,7 +735,7 @@ class EditingPanelFactory {
         layout.setVerticalGroup(layout.createSequentialGroup()
                 .addComponent(label)
                 .addComponent(p1Panel)
-                .addGap(0, 2, 2)
+                .addGap(2)
                 .addComponent(p2Panel));
 
         return panel;
@@ -509,11 +792,11 @@ class EditingPanelFactory {
         layout.setVerticalGroup(layout.createSequentialGroup()
                 .addComponent(label)
                 .addComponent(p1Panel)
-                .addGap(0, 2, 2)
+                .addGap(2)
                 .addComponent(p2Panel)
-                .addGap(0, 2, 2)
+                .addGap(2)
                 .addComponent(p3Panel)
-                .addGap(0, 2, 2)
+                .addGap(2)
                 .addComponent(p4Panel));
 
         return panel;
@@ -533,6 +816,39 @@ class EditingPanelFactory {
         layout.setVerticalGroup(layout.createSequentialGroup()
                 .addComponent(label)
                 .addComponent(pointPanel));
+
+        return panel;
+    }
+
+    public static JPanel create(GraphicImage image) {
+        JPanel panel = new JPanel();
+        GroupLayout layout = new GroupLayout(panel);
+        panel.setLayout(layout);
+
+        JLabel label = new JLabel("GraphicImage");
+
+        var filePathPanel = create("file path", image.filePath);
+
+        var originPanel = create("origin", image.origin);
+        var sizePanel = create("size", image.size);
+
+        var opacityPanel = create("opacity", image.opacity, 0.0, 1.0, 0.01);
+
+        layout.setHorizontalGroup(layout.createParallelGroup(Alignment.LEADING)
+                .addComponent(label)
+                .addComponent(filePathPanel)
+                .addComponent(originPanel)
+                .addComponent(sizePanel)
+                .addComponent(opacityPanel));
+        layout.setVerticalGroup(layout.createSequentialGroup()
+                .addComponent(label)
+                .addComponent(filePathPanel)
+                .addGap(2)
+                .addComponent(originPanel)
+                .addGap(2)
+                .addComponent(sizePanel)
+                .addGap(2)
+                .addComponent(opacityPanel));
 
         return panel;
     }
