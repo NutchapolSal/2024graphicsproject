78609688c16251573a70c88895928ce23c76674a
NutchapolSal <Nutchapol.sal@gmail.com>
2024-01-04 10:04:39 +0700

    set look and feel to system style java metal sucks

--- Main.java
+++ Main.java
@@ -60,11 +60,18 @@ import javax.swing.JTextField;
 import javax.swing.KeyStroke;
 import javax.swing.SpinnerNumberModel;
 import javax.swing.Timer;
+import javax.swing.UIManager;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.LayoutStyle.ComponentPlacement;
 
 public class Main {
     public static void main(String[] args) {
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         List<GraphicLayer> instructions = new ArrayList<>();
         instructions.add(new GraphicLayer("third point six swing")
