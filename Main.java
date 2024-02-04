1bb00ad887270e7242b5030bb92012079ef4cf42
NutchapolSal <Nutchapol.sal@gmail.com>
2024-02-04 19:40:48 +0700

    add interpolaty stuff

--- Main.java
+++ Main.java
@@ -28,9 +28,11 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.Scanner;
+import java.util.function.DoubleUnaryOperator;
 import java.util.function.Predicate;
 import java.util.prefs.Preferences;
 
@@ -67,7 +69,14 @@ import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 
 public class Main {
+
+    private static void runTests() {
+        AnimatedValue.test();
+        System.out.println();
+    }
+
     public static void main(String[] args) {
+        runTests();
 
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
@@ -373,6 +382,442 @@ class EditorFrame {
             layerI++;
         }
         layerScrollPane.setViewportView(layerPane);
+    }
+}
+
+class EasingFunctions {
+    private EasingFunctions() {
+    }
+
+    public static DoubleUnaryOperator linear() {
+        return x -> x;
+    }
+
+    public static DoubleUnaryOperator easeOutSine() {
+        return x -> Math.sin((x * Math.PI) / 2.0);
+    }
+
+    public static DoubleUnaryOperator easeInSine() {
+        return x -> 1 - Math.cos((x * Math.PI) / 2.0);
+    }
+
+    public static DoubleUnaryOperator easeInOutSine() {
+        return x -> -(Math.cos(Math.PI * x) - 1) / 2;
+    }
+
+    public static DoubleUnaryOperator easeInPower(double power) {
+        return x -> Math.pow(x, power);
+    }
+
+    public static DoubleUnaryOperator easeOutPower(double power) {
+        return x -> 1 - Math.pow((1 - x), power);
+    }
+
+    public static DoubleUnaryOperator easeInOutPower(double power) {
+        return x -> x < 0.5 ? 2 * Math.pow(x, power) : 1 - Math.pow(-2 * x + 2, 2) / 2;
+    }
+
+    public static DoubleUnaryOperator easeInExpo() {
+        return x -> x <= 0 ? 0 : Math.pow(2, 10 * x - 10);
+    }
+
+    public static DoubleUnaryOperator easeOutExpo() {
+        return x -> 1.0 <= x ? 1.0 : 1 - Math.pow(2, -10 * x);
+    }
+
+    public static DoubleUnaryOperator easeInOutExpo() {
+        return x -> {
+            if (x <= 0) {
+                return 0;
+            }
+            if (1 <= x) {
+                return 1;
+            }
+            return x < 0.5 ? Math.pow(2, 20 * x - 10) / 2
+                    : (2 - Math.pow(2, -20 * x + 10)) / 2;
+        };
+    }
+
+    public static DoubleUnaryOperator snap() {
+        return x -> x < 1 ? 0 : 1;
+    }
+}
+
+class AnimatedValue {
+    static class Timepoint {
+        public double time;
+        public DoubleUnaryOperator easingToNext;
+
+        Timepoint(double time, DoubleUnaryOperator easingToNext) {
+            this.time = time;
+            this.easingToNext = easingToNext;
+        }
+    }
+
+    static class StepValue {
+        public int index;
+        public double frac;
+
+        StepValue(int index, double frac) {
+            this.index = index;
+            this.frac = frac;
+        }
+
+        @Override
+        public String toString() {
+            return "StepValue [index=" + index + ", frac=" + frac + "]";
+        }
+    }
+
+    public List<Timepoint> timepoints = new ArrayList<>();
+
+    public boolean addTimepoint(double time, DoubleUnaryOperator easingToNext) {
+        int index = Collections.binarySearch(timepoints, new Timepoint(time, null),
+                (a, b) -> Double.compare(a.time, b.time));
+        if (index < 0) {
+            index = ~index;
+        } else {
+            return false;
+        }
+
+        timepoints.add(index, new Timepoint(time, easingToNext));
+        return true;
+        // timepoints.sort((a, b) -> Double.compare(a.time, b.time));
+    }
+
+    protected StepValue getValue(double time) {
+        int iBeforeTime = -1; // index of timepoint before or equal time
+        for (Timepoint timepoint : timepoints) {
+            if (timepoint.time <= time) {
+                iBeforeTime++;
+            } else {
+                break;
+            }
+        }
+
+        if (iBeforeTime == -1) {
+            return new StepValue(0, 0);
+        }
+        if (iBeforeTime == timepoints.size() - 1) {
+            return new StepValue(iBeforeTime, 0);
+        }
+
+        double timeBefore = timepoints.get(iBeforeTime).time;
+        double timeAfter = timepoints.get(iBeforeTime + 1).time;
+        double frac = (time - timeBefore) / (timeAfter - timeBefore);
+        return new StepValue(iBeforeTime, timepoints.get(iBeforeTime).easingToNext.applyAsDouble(frac));
+    }
+
+    public static void test() {
+        System.out.println("AnimatedValue test");
+        var animvalue = new AnimatedValue();
+        System.out.println(animvalue.getValue(0.5)); // 0, 0.0
+        System.out.println("---");
+        animvalue.addTimepoint(0, EasingFunctions.linear());
+        System.out.println(animvalue.getValue(-1)); // 0, 0.0
+        System.out.println(animvalue.getValue(0)); // 0, 0.0
+        System.out.println(animvalue.getValue(1)); // 0, 0.0
+        System.out.println("---");
+        animvalue.addTimepoint(1, EasingFunctions.linear());
+        System.out.println(animvalue.getValue(0)); // 0, 0.0
+        System.out.println(animvalue.getValue(0.33)); // 0, ~0.33
+        System.out.println(animvalue.getValue(0.66)); // 0, ~0.66
+        System.out.println(animvalue.getValue(1)); // 1, 0.0
+        System.out.println(animvalue.getValue(1.33)); // 1, 0.0
+        System.out.println("---");
+        animvalue.addTimepoint(3, EasingFunctions.linear());
+        System.out.println(animvalue.getValue(1)); // 1, 0.0
+        System.out.println(animvalue.getValue(1.66)); // 1, ~0.33
+        System.out.println(animvalue.getValue(2.33)); // 1, ~0.66
+        System.out.println(animvalue.getValue(3)); // 2, 0.0
+        System.out.println(animvalue.getValue(4)); // 2, 0.0
+        System.out.println("---");
+        animvalue = new AnimatedValue();
+        animvalue.addTimepoint(0, EasingFunctions.linear());
+        animvalue.addTimepoint(3, EasingFunctions.linear());
+        System.out.println(animvalue.getValue(1)); // 0, ~0.33
+        animvalue.addTimepoint(1, EasingFunctions.linear());
+        animvalue.addTimepoint(1, EasingFunctions.linear());
+        animvalue.addTimepoint(1, EasingFunctions.linear());
+        System.out.println(animvalue.getValue(1)); // 1, 0.0
+    }
+}
+
+class AnimBoolean extends AnimatedValue {
+    public List<Boolean> valuepoints = new ArrayList<>();
+
+    public void add(double time, boolean value, DoubleUnaryOperator easingToNext) {
+        super.addTimepoint(time, EasingFunctions.snap());
+        valuepoints.add(value);
+    }
+
+    public void remove(double time) {
+        var stepValue = getValue(time);
+        valuepoints.remove(stepValue.index);
+        timepoints.remove(stepValue.index);
+    }
+
+    public boolean get(double time) {
+        if (timepoints.isEmpty()) {
+            return false;
+        }
+        var stepValue = getValue(time);
+
+        return Lerp.run(valuepoints.get(stepValue.index), stepValue.frac, valuepoints.get(stepValue.index + 1));
+    }
+}
+
+class AnimDouble extends AnimatedValue {
+    public List<Double> valuepoints = new ArrayList<>();
+
+    public void add(double time, double value, DoubleUnaryOperator easingToNext) {
+        super.addTimepoint(time, easingToNext);
+        valuepoints.add(value);
+    }
+
+    public void remove(double time) {
+        var stepValue = getValue(time);
+        valuepoints.remove(stepValue.index);
+        timepoints.remove(stepValue.index);
+    }
+
+    public double get(double time) {
+        if (timepoints.isEmpty()) {
+            return 0;
+        }
+        var stepValue = getValue(time);
+
+        return Lerp.run(valuepoints.get(stepValue.index), stepValue.frac, valuepoints.get(stepValue.index + 1));
+    }
+}
+
+class AnimInt extends AnimatedValue {
+    public List<Integer> valuepoints = new ArrayList<>();
+
+    public void add(double time, int value, DoubleUnaryOperator easingToNext) {
+        super.addTimepoint(time, easingToNext);
+        valuepoints.add(value);
+    }
+
+    public void remove(double time) {
+        var stepValue = getValue(time);
+        valuepoints.remove(stepValue.index);
+        timepoints.remove(stepValue.index);
+    }
+
+    public int get(double time) {
+        if (timepoints.isEmpty()) {
+            return 0;
+        }
+        var stepValue = getValue(time);
+
+        return Lerp.run(valuepoints.get(stepValue.index), stepValue.frac, valuepoints.get(stepValue.index + 1));
+    }
+}
+
+class AnimColor extends AnimatedValue {
+    public List<Color> valuepoints = new ArrayList<>();
+
+    public void add(double time, Color value, DoubleUnaryOperator easingToNext) {
+        super.addTimepoint(time, easingToNext);
+        valuepoints.add(value);
+    }
+
+    public void remove(double time) {
+        var stepValue = getValue(time);
+        valuepoints.remove(stepValue.index);
+        timepoints.remove(stepValue.index);
+    }
+
+    public Color get(double time) {
+        if (timepoints.isEmpty()) {
+            return Color.black;
+        }
+        var stepValue = getValue(time);
+
+        return Lerp.run(valuepoints.get(stepValue.index), stepValue.frac, valuepoints.get(stepValue.index + 1));
+    }
+}
+
+class Lerp {
+    public static boolean run(boolean before, double frac, boolean after) {
+        if (frac <= 0) {
+            return before;
+        }
+        if (frac >= 1) {
+            return after;
+        }
+
+        return before;
+    }
+
+    public static double run(double before, double frac, double after) {
+        if (frac <= 0) {
+            return before;
+        }
+        if (frac >= 1) {
+            return after;
+        }
+
+        return before + (after - before) * frac;
+    }
+
+    public static int run(int before, double frac, int after) {
+        if (frac <= 0) {
+            return before;
+        }
+        if (frac >= 1) {
+            return after;
+        }
+
+        return (int) (before + (after - before) * frac);
+    }
+
+    public static float run(float before, double frac, float after) {
+        if (frac <= 0) {
+            return before;
+        }
+        if (frac >= 1) {
+            return after;
+        }
+
+        return (float) (before + (after - before) * frac);
+    }
+
+    // BRACE YOURSELF FOR COLOR PROCESSING
+
+    // https://bottosson.github.io/posts/colorwrong/#what-can-we-do%3F
+    // https://bottosson.github.io/posts/oklab/
+    private static class LinearSRGB {
+        public float r;
+        public float g;
+        public float b;
+
+        public LinearSRGB(float r, float g, float b) {
+            this.r = r;
+            this.g = g;
+            this.b = b;
+        }
+
+        private static float linearToSRGB(float c) {
+            if (c >= 0.0031308) {
+                return (1.055f) * (float) Math.pow(c, 1.0f / 2.4f) - 0.055f;
+            } else {
+                return 12.92f * c;
+            }
+
+        }
+
+        private static float sRGBToLinear(float c) {
+            if (c >= 0.04045) {
+                return (float) Math.pow(((c + 0.055) / (1 + 0.055)), 2.4);
+            } else {
+                return c / 12.92f;
+            }
+        }
+
+        private static float clamp(float c) {
+            return Math.min(Math.max(0, c), 1);
+        }
+
+        public LinearSRGB(Color c) {
+            this.r = sRGBToLinear(c.getRed() / 255.0f);
+            this.g = sRGBToLinear(c.getGreen() / 255.0f);
+            this.b = sRGBToLinear(c.getBlue() / 255.0f);
+        }
+
+        public LinearSRGB(OkLab c) {
+            float l_ = c.L + 0.3963377774f * c.a + 0.2158037573f * c.b;
+            float m_ = c.L - 0.1055613458f * c.a - 0.0638541728f * c.b;
+            float s_ = c.L - 0.0894841775f * c.a - 1.2914855480f * c.b;
+
+            float l = l_ * l_ * l_;
+            float m = m_ * m_ * m_;
+            float s = s_ * s_ * s_;
+
+            this.r = +4.0767416621f * l - 3.3077115913f * m + 0.2309699292f * s;
+            this.g = -1.2684380046f * l + 2.6097574011f * m - 0.3413193965f * s;
+            this.b = -0.0041960863f * l - 0.7034186147f * m + 1.7076147010f * s;
+        }
+
+        public Color toColor() {
+            return new Color(
+                    clamp(linearToSRGB(r)),
+                    clamp(linearToSRGB(g)),
+                    clamp(linearToSRGB(b)));
+        }
+    }
+
+    private static class OkLab {
+        public float L;
+        public float a;
+        public float b;
+
+        public OkLab(float L, float a, float b) {
+            this.L = L;
+            this.a = a;
+            this.b = b;
+        }
+
+        public OkLab(LinearSRGB c) {
+            float l = 0.4122214708f * c.r + 0.5363325363f * c.g + 0.0514459929f * c.b;
+            float m = 0.2119034982f * c.r + 0.6806995451f * c.g + 0.1073969566f * c.b;
+            float s = 0.0883024619f * c.r + 0.2817188376f * c.g + 0.6299787005f * c.b;
+
+            float l_ = (float) Math.cbrt(l);
+            float m_ = (float) Math.cbrt(m);
+            float s_ = (float) Math.cbrt(s);
+
+            this.L = 0.2104542553f * l_ + 0.7936177850f * m_ - 0.0040720468f * s_;
+            this.a = 1.9779984951f * l_ - 2.4285922050f * m_ + 0.4505937099f * s_;
+            this.b = 0.0259040371f * l_ + 0.7827717662f * m_ - 0.8086757660f * s_;
+        }
+
+        public OkLab multiplyAlpha(float alpha) {
+            this.L *= alpha;
+            this.a *= alpha;
+            this.b *= alpha;
+            return this;
+        }
+
+        public OkLab divideAlpha(float alpha) {
+            if (alpha == 0) {
+                this.L = 0;
+                this.a = 0;
+                this.b = 0;
+                return this;
+            }
+            this.L /= alpha;
+            this.a /= alpha;
+            this.b /= alpha;
+            return this;
+        }
+    }
+
+    /**
+     * lerps in OkLab color space + correct handling of alpha
+     */
+    public static Color run(Color before, double frac, Color after) {
+        if (frac <= 0) {
+            return before;
+        }
+        if (frac >= 1) {
+            return after;
+        }
+
+        var beforeOk = new OkLab(new LinearSRGB(before)).multiplyAlpha(before.getAlpha() / 255.0f);
+        var afterOk = new OkLab(new LinearSRGB(after)).multiplyAlpha(after.getAlpha() / 255.0f);
+        var lerpAlpha = run(before.getAlpha(), frac, after.getAlpha());
+        var lerpOk = new OkLab(
+                Lerp.run(beforeOk.L, frac, afterOk.L),
+                Lerp.run(beforeOk.a, frac, afterOk.a),
+                Lerp.run(beforeOk.b, frac, afterOk.b))
+                .divideAlpha(lerpAlpha / 255.0f);
+        var lerp = new LinearSRGB(lerpOk).toColor();
+
+        return new Color(lerp.getRed(), lerp.getGreen(), lerp.getBlue(),
+                lerpAlpha);
+
     }
 }
 
