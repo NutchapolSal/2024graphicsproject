e8d79695028f16a77ed506ce403144e7f5f4ff06
NutchapolSal <Nutchapol.sal@gmail.com>
2024-02-06 16:06:26 +0700

    change center to middle of pane

--- Main.java
+++ Main.java
@@ -3133,23 +3133,27 @@ class GraphicsPanel extends JPanel {
     @Override
     protected void paintComponent(Graphics gOuter) {
         super.paintComponent(gOuter);
+        var size = this.getSize();
         Graphics g = gOuter.create();
-        // g.translate(300, 300);
+        g.translate(size.width / 2, size.height / 2);
         var debugG = (Graphics2D) gOuter.create();
         debugG.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        // debugG.translate(300, 300);
-
-        debugG.setColor(Color.red);
-        // debugG.drawRect(-301, -301, 601, 601);
-        debugG.fillRect(0, 0, 1, 1);
-        debugG.fillRect(599, 589, 1, 1);
-        debugG.fillRect(589, 599, 1, 1);
+        debugG.translate(size.width / 2, size.height / 2);
 
         for (GraphicLayer layer : instructions) {
             if (layer.shown.get(time)) {
                 layer.draw(g, time);
             }
         }
+
+        for (int i = 0; i < 600; i++) {
+            debugG.setColor(i % 2 == 0 ? Color.white : Color.black);
+            debugG.fillRect(-301 + i, -305, 1, 5);
+            debugG.fillRect(-305, -301 + i, 5, 1);
+            debugG.fillRect(300 - i, 300, 1, 5);
+            debugG.fillRect(300, 300 - i, 5, 1);
+        }
+
         if (!GlobalState.pannerPanelDragging || GlobalState.pannerShowDebugging) {
             for (GraphicLayer layer : instructions) {
                 layer.debugDraw(debugG, time);
