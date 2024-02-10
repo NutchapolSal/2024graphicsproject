d0cb80b91999190c4aff368f9f54abb1e79c0c55
NutchapolSal <Nutchapol.sal@gmail.com>
2024-02-11 00:04:26 +0700

    add editing palette values

--- PaletteSlotPanel.java
+++ PaletteSlotPanel.java
@@ -5,8 +5,12 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
 
 import javax.swing.BorderFactory;
+import javax.swing.JColorChooser;
 import javax.swing.JComponent;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.TransferHandler;
 
 public class PaletteSlotPanel extends JPanel {
@@ -14,12 +18,14 @@ public class PaletteSlotPanel extends JPanel {
     private int x;
     private int y;
 
+    private JPopupMenu popup = new JPopupMenu();
+
     PaletteSlotPanel(Palette palette, int x, int y, PaletteSlotTransferHandler transferHandler) {
         super();
         this.palette = palette;
         this.x = x;
         this.y = y;
-        updatePanel();
+
         this.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 
         this.setPreferredSize(new Dimension(25, 25));
@@ -29,12 +35,39 @@ public class PaletteSlotPanel extends JPanel {
         this.putClientProperty("canPaletteValueMove", true);
         this.addMouseMotionListener(new MouseMotionAdapter() {
             public void mouseDragged(MouseEvent e) {
+                if (MouseEvent.BUTTON1 != e.getButton()) {
+                    return;
+                }
                 JComponent c = (JComponent) e.getSource();
                 TransferHandler handler = c.getTransferHandler();
-                // handler.exportAsDrag(c, e, handler.getSourceActions(c));
                 handler.exportAsDrag(c, e, TransferHandler.MOVE);
             }
         });
+        JMenuItem editColorItem = popup.add("Edit Color");
+        JMenuItem editLabel = popup.add("Edit Label");
+
+        editColorItem.addActionListener(e -> {
+            var value = palette.get(x, y).get();
+            var color = value.color;
+            var newColor = JColorChooser.showDialog(this, "Choose Color", color);
+            if (newColor != null) {
+                value.color = newColor;
+                updatePanel();
+            }
+        });
+
+        editLabel.addActionListener(e -> {
+            var value = palette.get(x, y).get();
+            var label = value.label;
+            var newLabel = (String) JOptionPane.showInputDialog(this, "Enter Label", "Edit Label",
+                    javax.swing.JOptionPane.PLAIN_MESSAGE, null, null, label);
+            if (newLabel != null) {
+                value.label = newLabel;
+                updatePanel();
+            }
+        });
+
+        updatePanel();
     }
 
     private void updatePanel() {
@@ -44,11 +77,13 @@ public class PaletteSlotPanel extends JPanel {
             this.setCursor(new Cursor(Cursor.MOVE_CURSOR));
             this.setToolTipText("<html>" + ColorHexer.encode(value.get().color) + "<br>" + value.get().label + "<br>"
                     + value.get().id);
+            this.setComponentPopupMenu(popup);
             this.putClientProperty("paletteValue", value.get());
         } else {
             this.setBackground(null);
             this.setCursor(null);
             this.setToolTipText(null);
+            this.setComponentPopupMenu(null);
             this.putClientProperty("paletteValue", null);
         }
     }
